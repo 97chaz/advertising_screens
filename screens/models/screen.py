78fd4e57c19cc395a89e0ff3d74e28249403c50e
97chaz <73785115+97chaz@users.noreply.github.com>
2021-02-19 17:52:01 +0000

    Added displaying of when screens where last online

--- screens/models/screen.py
+++ screens/models/screen.py
@@ -1,5 +1,7 @@
 from django.db import models
 from django.urls import reverse
+from datetime import datetime, timedelta
+from django.db.models import Q
 
 from screens.models import Source
 from screens.models.schedule import Schedule
@@ -15,6 +17,12 @@ class Screen(models.Model):
                                             blank=True,
                                             help_text="Optional. Plays randomly between other content")
     ip = models.GenericIPAddressField()
+    last_seen = models.DateTimeField()
+    
+    def online(self):
+        return self.last_seen and self.last_seen >= datetime.now()-timedelta(minutes=1)
+    online.boolean = True
+        
 
     def __str__(self):
         return self.name
